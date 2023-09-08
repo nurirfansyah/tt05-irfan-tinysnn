bd3c24546315da1cd1804a2e212711366412cef4
nurirfansyah <40015347+nurirfansyah@users.noreply.github.com>
2023-09-09 05:22:24 +0700

    Update EncoderNeuron.v

--- src/EncoderNeuron.v
+++ src/EncoderNeuron.v
@@ -9,7 +9,7 @@ module EncoderNeuron (
     parameter M = 5;  
 
     reg [3:0] counter;  
-    reg [3:0] lfsr = 4'b0001;  // LFSR
+    reg [3:0] lfsr;  // Removed the initialization here
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -28,15 +28,16 @@ module EncoderNeuron (
                 if (counter == (M - 1) - lfsr[0]) begin
                     spikeOutput <= 1;
                     counter <= 0;
-                end else
+                end 
+                else
                     counter <= counter + 1;
-            end else
-                if (counter == (N - 1) - lfsr[0]) begin
-                    spikeOutput <= 1;
-                    counter <= 0;
-                end else
-                    counter <= counter + 1;
-            end
+            end 
+            else if (counter == (N - 1) - lfsr[0]) begin
+                spikeOutput <= 1;
+                counter <= 0;
+            end 
+            else
+                counter <= counter + 1;
         end
     end
 
